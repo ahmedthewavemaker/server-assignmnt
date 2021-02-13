a5a10b5fa8b28bb101de724ae21b23c94b79a70c
ahmedthewavemaker <72584721+ahmedthewavemaker@users.noreply.github.com>
2021-02-12 20:54:46 -0500

    backend

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
-const express= require('express');
-const morgan= require('morgan');
-const cors=require('cors');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
-const app=express();
+const app = express();
 
 app.use(morgan('common'));
 app.use(cors());
 
-const play=require('./playServer-data.js');
+const play = require('./playServer-data.js');
 
-app.get('/apps', (req, res)=>{
-    const {sort, genres}=req.query;
-    if(sort){
-        if(!['rating', 'app'].includes(sort)){
+app.get('/apps', (req, res) => {
+    const { sort, genres } = req.query;
+    let results=play
+console.log(results)
+    if (sort) {
+        if (!['Rating', 'App'].includes(sort)) {
             return res
-                    .status(400)
-                    .send('Sort must be of Rating or App');
+                .status(400)
+                .send('Sort must be of Rating or App');
         }
-    
-    if(genres){
-        if(!['Action', 'Puzzle', 'Arcade', 'Strategy','Casual', 'Card'].includes(genres)){
+        results=results.sort((a,b)=> a[sort]>b[sort]?-1: 1);
+    }
+    if (genres) {
+        if (!['Action', 'Puzzle', 'Arcade', 'Strategy', 'Casual', 'Card'].includes(genres)) {
             return res
-                    .status(400)
-                    .send('Genres must be either Action, Puzzle, Arcade, Strategy, Casual, or Card');
-    
-        }}
+                .status(400)
+                .send('Genres must be either Action, Puzzle, Arcade, Strategy, Casual, or Card');
 
+        }
+        results=results.filter(item => item.Genres==genres)
     }
 
-res.json(play)
+
+
+
+
+    res.json(results)
 })
 
 
-app.listen(8000, ()=> {
+app.listen(8000, () => {
     console.log('server listening at 8000 boiiiii')
 })
